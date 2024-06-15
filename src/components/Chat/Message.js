7ec33f4b58872afb7b8a428b5a9ab1dc834c2ad0
AldG99 <a.garciaglz17@gmail.com>
2024-06-15 16:23:17 -0600

    Modificar archivo Message.js

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -1,6 +1,11 @@
+import React from "react";
 
-const Message = () => {
-
+const Message = ({ message }) => {
+  return (
+    <div>
+      <p><strong>{message.sender}:</strong> {message.text}</p>
+    </div>
+  );
 };
 
 export default Message;
