f807c040b8c939ac975630fe951d6f335b5f20a6
AldG99 <a.garciaglz17@gmail.com>
2024-06-17 22:36:12 -0600

    Modificar código del archivo Profile.js

--- src/components/Auth/Profile.js
+++ src/components/Auth/Profile.js
@@ -2,23 +2,108 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { storage, auth, firestore } from "../../firebaseConfig";
 import { signOut } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc, query, where, getDocs, collection, setDoc } from "firebase/firestore";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const [userData, setUserData] = useState(null);
+  const [contacts, setContacts] = useState([]);
+  const [friendRequests, setFriendRequests] = useState([]);
   const [image, setImage] = useState(null);
   const [url, setUrl] = useState(auth.currentUser.phoneURL || "https://via.placeholder.com/150");
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUserData = async () => {
-      const userDoc = await getDoc(doc(firestore, "users", auth.currentUser.uid));
-      if (userDoc.exists()) {
-        setUserData(userDoc.data());
+      try {
+        const userDoc = await getDoc(doc(firestore, "users", auth.currentUser.uid));
+        if (userDoc.exists()) {
+          setUserData(userDoc.data());
+        }
+      } catch (error) {
+        console.error("Error fetching user data", error);
       }
     };
+
+    const fetchContacts = async () => {
+      try {
+        const contactsDoc = await getDoc(doc(firestore, "contacts", auth.currentUser.uid));
+        if (contactsDoc.exists()) {
+          const contactIds = contactsDoc.data().contacts || [];
+          const contactPromises = contactIds.map(id => getDoc(doc(firestore, "users", id)));
+          const contactSnapshots = await Promise.all(contactPromises);
+          const contactData = contactSnapshots.map(snap => snap.data());
+          setContacts(contactData);
+        }
+      } catch (error) {
+        console.error("Error fetching contacts:", error);
+      }
+    };
+
+    const fetchFriendRequests = async () => {
+      try {
+        const q = query(
+          collection(firestore, "friendRequests"),
+          where("to", "==", auth.currentUser.uid),
+          where("status", "==", "pending")
+        );
+        const querySnapshot = await getDocs(q);
+        const requests = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setFriendRequests(requests);
+      } catch (error) {
+        console.error("Error fetching friend requests:", error);
+      }
+    };
+
     fetchUserData();
+    fetchContacts();
+    fetchFriendRequests();
   }, []);
+
+  const handleAcceptRequest = async (requestId, fromUserId) => {
+    try {
+      const userContactsDocRef = doc(firestore, "contacts", auth.currentUser.uid);
+      const userDocSnap = await getDoc(userContactsDocRef);
+      const userContacts = userDocSnap.exists() ? userDocSnap.data().contacts || [] : [];
+
+      if (userDocSnap.exists()) {
+        await updateDoc(userContactsDocRef, {
+          contacts: [...userContacts, fromUserId]
+        });
+      } else {
+        await setDoc(userContactsDocRef, {
+          contacts: [fromUserId]
+        });
+      }
+
+      const friendContactsDocRef = doc(firestore, "contacts", fromUserId);
+      const friendDocSnap = await getDoc(friendContactsDocRef);
+      const friendContacts = friendDocSnap.exists() ? friendDocSnap.data().contact || [] : [];
+
+      if (friendDocSnap.exists()) {
+        await updateDoc(friendContactsDocRef, {
+          contacts: [...friendContacts, auth.currentUser.uid]
+        });
+      } else {
+        await setDoc(friendContactsDocRef, {
+          contacts: [auth.currentUser.uid]
+        });
+      }
+
+      const requestDocRef = doc(firestore, "friendRequests", requestId);
+      await updateDoc(requestDocRef, { status: "accepted"});
+      
+      const newContactDoc = await getDoc(doc(firestore, "users", fromUserId));
+      const newContactData = newContactDoc.data();
+      setContacts([...contacts, newContactData]);
+
+      setFriendRequests(friendRequests.filter(request => request.id !== requestId));
+    } catch (error) {
+      console.error("Error al aceptar la solicitud de amistad:", error);
+    }
+  };
 
   const handleChange = (e) => {
     if (e.target.files[0]) {
@@ -27,22 +112,21 @@ const Profile = () => {
   };
 
   const handleUpload = () => {
-    const uploadTansk = storage.ref(`images/${auth.currentUser.uid}`).put(image);
-    uploadTansk.on(
+    if (!image) return;
+
+    const storageRef = ref(storage, `images/${auth.currentUser.uid}`);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+
+    uploadTask.on(
       "state_changed",
-      (snapshot) => {},
       (error) => {
         console.log(error);
       },
       () => {
-        storage
-          .ref("images")
-          .child(auth.currentUser.uid)
-          .getDownloadURL()
-          .then((url) => {
-            setUrl(url);
-            auth.currentUser.uploadProfile({ photoURL: url });
-          });
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setUrl(downloadURL);
+          updateProfile(auth.currentUser, { photoURL: downloadURL });
+        });
       }
     );
   };
@@ -56,14 +140,21 @@ const Profile = () => {
     }
   };
 
+  const calculateAge = (birthdate) => {
+    const birthDate = new Date(birthdate);
+    const ageDifMs = Date.now() - birthDate.getTime();
+    const ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  };
+
   return (
     <div>
       <h2>Perfil</h2>
       {userData ? (
         <div>
           <p>Nombre: {userData.firstName} {userData.lastName}</p>
-          <p>Edad: {userData.age}</p>
-          <p>Género: {userData.gender}</p>
+          <p>Edad: {calculateAge(userData.birthDate)}</p>
+          <p>ID Única: {userData.userId}</p>
         </div>
       ) : (
         <p>Cargando información del usuario...</p>
@@ -71,8 +162,34 @@ const Profile = () => {
       <p>Correo electrónico: {auth.currentUser.email}</p>
       <input type="file" onChange={handleChange} />
       <button onClick={handleUpload}>Subir Foto</button>
-      <img src={url} alt="Profile" />
+      <img src={url} alt="Profile" style={{ width: "150px", height: "150px", objectFit: "cover" }}/>
       <button onClick={handleLogout}>Cerrar sesión</button>
+
+      <h3>Contactos</h3>
+      {contacts.length > 0 ? (
+        <ul>
+          {contacts.map((contact, index) => (
+            <li key={index}>{contact.firstName} {contact.lastName}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No tienes contactos aún.</p>
+      )}
+
+      <h3>Solicitud de Amistad</h3>
+      {friendRequests.length > 0 ? (
+        <ul>
+          {friendRequests.map(request => (
+            <li key={request.id}>
+              {request.fromUserName} ({request.fromUserId})
+              <button onClick={() => handleAcceptRequest(request.id, request.from)}>Aceptar</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No tienes solicitudes de amistad pendientes.</p>
+      )}
+      <Link to="/add-contact">Añadir Contacto</Link>
     </div>
   );
 };
