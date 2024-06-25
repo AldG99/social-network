f52945a8a8fb93cf0d298b0173ff12b958b8c2b8
AldG99 <a.garciaglz17@gmail.com>
2024-06-25 17:50:54 -0600

    Código archivos Mensaje

---
+++ src/components/Chat/Messages.js
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../../firebaseConfig";
+import Message from "./Message";
+
+const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [data.chatId]);
+
+  console.log(messages)
+
+  return (
+    <div className="messages">
+      {messages.map((m) => (
+        <Message message={m} key={m.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Messages;
